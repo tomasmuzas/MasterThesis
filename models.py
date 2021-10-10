0fedbbc6927aceb9002cbeb5e886a61cb22d66ae
Tomas Mūžas <m_tomas@inbox.lt>
2021-10-10 18:03:31 +0300

    Add Custom4 model

--- models.py
+++ models.py
@@ -101,6 +101,21 @@ def Custom6Model(classes, image_size, dropout_rate):
   model.add(Dense(classes, activation='softmax'))
   return model
 
+def Custom4Model(classes, image_size, dropout_rate):
+  model = Sequential(name="Custom6")
+  model.add(Input(shape=(image_size, image_size, 3)))
+  model.add(Conv2D(filters=64, kernel_size=7, strides=2, padding='same', name="Conv1"))   
+  model.add(MaxPool2D(3, strides=2, padding='same', name="Conv2_MaxPool"))
+  model.add(ConvBlockNoResidual(64, 1, image_size // 4, initial_stride = 1, pad_input = False, name="Conv2"))
+  if dropout_rate != 0:
+    model.add(Dropout(dropout_rate))
+  model.add(ConvBlockNoResidual(128, 1, image_size // 4, initial_stride = 2, pad_input = True, name="Conv3"))
+  if dropout_rate != 0:
+    model.add(Dropout(dropout_rate))
+  model.add(GlobalAveragePooling2D())
+  model.add(Dense(classes, activation='softmax'))
+  return model
+
 def ResNet6Model(classes, image_size, dropout_rate):
   model = Sequential(name="Custom6")
   model.add(Input(shape=(image_size, image_size, 3)))
@@ -160,6 +175,9 @@ def ResNet18Model(classes, image_size, dropout_rate):
   model.add(GlobalAveragePooling2D())
   model.add(Dense(classes, activation='softmax'))
   return model
+
+def create_Custom4_model(classes, image_size, dropout_rate):
+  return Custom4Model(classes=classes, image_size= image_size, dropout_rate= dropout_rate)
 
 def create_Custom6_model(classes, image_size, dropout_rate):
   return Custom6Model(classes=classes, image_size= image_size, dropout_rate= dropout_rate)
