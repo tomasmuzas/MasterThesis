3aea6c35532e3fb94d896f1fc1d9d96fa8cecc9b
Tomas Mūžas <m_tomas@inbox.lt>
2021-10-24 13:15:54 +0300

    Add augmentation into dataset before batching

--- read_dataset.py
+++ read_dataset.py
@@ -29,12 +29,44 @@ def read_tfrecord(example, image_size):
     one_hot_class = tf.sparse.to_dense(example['one_hot_class'])
     return image, class_num, label, height, width, one_hot_class
 
-def read_tf_record_dataset(path, name, preprocessing_function, image_size, batch_size):
+@tf.function
+def random_invert_horizontally(x, p=0.5):
+  if  tf.random.uniform([]) < p:
+    x = tf.image.flip_left_right(x)
+  else:
+    x
+  return x
+
+@tf.function
+def random_invert_vertically(x, p=0.5):
+  if  tf.random.uniform([]) < p:
+    x = tf.image.flip_up_down(x)
+  else:
+    x
+  return x
+
+@tf.function
+def random_rotate(x, p=0.5):
+  if  tf.random.uniform([]) < p:
+    x = tf.image.rot90(x, k = tf.random.uniform([], minval=1, maxval=4, dtype=tf.int32))
+  else:
+    x
+  return x
+
+RandomHorizontalFlip = tf.keras.layers.Lambda(lambda x: random_invert_horizontally(x))
+RandomVerticalFlip = tf.keras.layers.Lambda(lambda x: random_invert_vertically(x))
+RandomRotate = tf.keras.layers.Lambda(lambda x: random_rotate(x))
+
+def read_tf_record_dataset(path, name, preprocessing_function, image_size, batch_size, augment = False):
   filenames = tf.io.gfile.glob(path + "/{}/*.tfrec".format(name))
   dataset4 = tf.data.TFRecordDataset(filenames, num_parallel_reads=AUTO)
   dataset4 = dataset4.map(lambda x: read_tfrecord(x, image_size), num_parallel_calls=AUTO)
   dataset4 = dataset4.map(lambda image, class_num, label, height, width, one_hot_class: (image, one_hot_class))
   dataset4 = dataset4.map(lambda x, y: (tf.cast(x, tf.float32), y))
   dataset4 = dataset4.map(lambda x, y: (preprocessing_function(x), y))
+  if(augment):
+    dataset4.map(RandomHorizontalFlip, num_parallel_calls=AUTO)
+    dataset4.map(RandomVerticalFlip, num_parallel_calls=AUTO)
+    dataset4.map(RandomRotate, num_parallel_calls=AUTO)
 
   return dataset4.batch(batch_size, drop_remainder=True).prefetch(AUTO)
