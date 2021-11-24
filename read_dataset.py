4c7939a5cc5165058a6a07b6cd49c758542a8a0e
Tomas Mūžas <m_tomas@inbox.lt>
2021-11-24 20:02:38 +0200

    Update read_dataset for the new structure

--- read_dataset.py
+++ read_dataset.py
@@ -9,7 +9,7 @@ def read_tfrecord(example, image_size):
         
         # additional (not very useful) fields to demonstrate TFRecord writing/reading of different types of data
         "label":         tf.io.FixedLenFeature([], tf.string),  # one bytestring
-        "size":          tf.io.FixedLenFeature([2], tf.int64),  # two integers
+        "objid":         tf.io.FixedLenFeature([], tf.string),  # two integers
         "one_hot_class": tf.io.VarLenFeature(tf.float32)        # a certain number of floats
     }
     # decode the TFRecord
@@ -24,10 +24,9 @@ def read_tfrecord(example, image_size):
     class_num = example['class']
     
     label  = example['label']
-    height = example['size'][0]
-    width  = example['size'][1]
+    objid = example['objid']
     one_hot_class = tf.sparse.to_dense(example['one_hot_class'])
-    return image, class_num, label, height, width, one_hot_class
+    return image, class_num, label, objid, one_hot_class
 
 @tf.function
 def random_invert_horizontally(x, p=0.5):
@@ -53,16 +52,25 @@ def random_rotate(x, p=0.5):
     x
   return x
 
-def read_tf_record_dataset(path, name, preprocessing_function, image_size, batch_size, augment = False):
-  filenames = tf.io.gfile.glob(path + "/{}/*.tfrec".format(name))
+def read_tf_record_dataset(path, preprocessing_function, image_size, batch_size, augment = False, include_objid = False):
+  filenames = tf.io.gfile.glob(path + "/*.tfrec")
   dataset4 = tf.data.TFRecordDataset(filenames, num_parallel_reads=AUTO)
   dataset4 = dataset4.map(lambda x: read_tfrecord(x, image_size), num_parallel_calls=AUTO)
-  dataset4 = dataset4.map(lambda image, class_num, label, height, width, one_hot_class: (image, class_num))
-  dataset4 = dataset4.map(lambda x, y: (tf.cast(x, tf.float32), y), num_parallel_calls=AUTO)
-  dataset4 = dataset4.map(lambda x, y: (preprocessing_function(x), y), num_parallel_calls=AUTO)
-  if(augment):
-    dataset4 = dataset4.map(lambda x,y : (random_invert_horizontally(x), y), num_parallel_calls=AUTO)
-    dataset4 = dataset4.map(lambda x,y : (random_invert_vertically(x), y), num_parallel_calls=AUTO)
-    dataset4 = dataset4.map(lambda x,y : (random_rotate(x), y), num_parallel_calls=AUTO)
+  if (include_objid):
+    dataset4 = dataset4.map(lambda image, class_num, label, objid, one_hot_class: (image, class_num, objid))
+    dataset4 = dataset4.map(lambda x, y, z: (tf.cast(x, tf.float32), y, z), num_parallel_calls=AUTO)
+    dataset4 = dataset4.map(lambda x, y, z: (preprocessing_function(x), y, z), num_parallel_calls=AUTO)
+    if(augment):
+      dataset4 = dataset4.map(lambda x,y,z : (random_invert_horizontally(x), y, z), num_parallel_calls=AUTO)
+      dataset4 = dataset4.map(lambda x,y,z : (random_invert_vertically(x), y, z), num_parallel_calls=AUTO)
+      dataset4 = dataset4.map(lambda x,y,z : (random_rotate(x), y, z), num_parallel_calls=AUTO)
+  else:
+    dataset4 = dataset4.map(lambda image, class_num, label, objid, one_hot_class: (image, class_num))
+    dataset4 = dataset4.map(lambda x, y: (tf.cast(x, tf.float32), y), num_parallel_calls=AUTO)
+    dataset4 = dataset4.map(lambda x, y: (preprocessing_function(x), y), num_parallel_calls=AUTO)
+    if(augment):
+      dataset4 = dataset4.map(lambda x,y : (random_invert_horizontally(x), y), num_parallel_calls=AUTO)
+      dataset4 = dataset4.map(lambda x,y : (random_invert_vertically(x), y), num_parallel_calls=AUTO)
+      dataset4 = dataset4.map(lambda x,y : (random_rotate(x), y), num_parallel_calls=AUTO)
 
   return dataset4.batch(batch_size, drop_remainder=True).prefetch(AUTO)
